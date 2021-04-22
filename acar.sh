41ee12becd01fa81d95f2417a7d40470d7d9ae97
Wotifrosty <53178032+Ege135@users.noreply.github.com>
2021-04-22 12:41:38 +0300

    Update acar.sh

--- acar.sh
+++ acar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while true ; do
 
-echo "[ACAR Moderasyon Botu] Beklenmedik bir hata olustu."
+echo "[Frosty Moderasyon Botu] Beklenmedik bir hata olustu."
 sleep 1
-echo "[ACAR Moderasyon Botu] Sistem tarafindan tekrardan baslatildi."
+echo "[Frosty Moderasyon Botu] Sistem tarafindan tekrardan baslatildi."
 
 node --optimize_for_size --max-old-space-size=8192 acar.js
 done
